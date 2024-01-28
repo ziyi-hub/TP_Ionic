35a35b8cbfcd5f8286370ab44d006609c45044f3
Ziyi WANG <ziyiwang1027@gmail.com>
2024-01-29 00:19:08 +0100

    fix: ion-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { TopicModalComponent } from './components/topic-modal/topic-modal.component';
 import { PostModalComponent } from './components/post-modal/post-modal.component';
+import { ToastComponent } from "./components/toast/toast.component";
 import { AppRoutingModule } from './app-routing.module';
 import { TopicDetailPageModule } from './topic-detail/topic-detail.module';
 import { HomePage } from "./home/home.page";
 
 @NgModule({
-  declarations: [AppComponent, TopicModalComponent, PostModalComponent, HomePage],
+  declarations: [AppComponent, TopicModalComponent, PostModalComponent, ToastComponent, HomePage],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, TopicDetailPageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
